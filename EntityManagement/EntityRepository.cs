4a217c791863c092043a743f65e7c51223ef96c1
Saji Weerasingham <TheMagnificent11@users.noreply.github.com>
2018-12-31 10:32:14 +1000

    moved most of the interfaces to a new entity manangement abstractions nuget package (and setters removed from entity interfaces)

--- EntityManagement/EntityRepository.cs
+++ EntityManagement/EntityRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using EntityManagement.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntityManagement
