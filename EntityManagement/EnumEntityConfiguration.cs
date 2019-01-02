5cb3c4eea12cc57cbb08d6c2e7137a2e04649f62
Saji Weerasingham <saji.weerasingham@outlook.com>
2019-01-03 08:34:33 +1000

    added enum entity configuration

---
+++ EntityManagement/EnumEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EntityManagement.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityManagement
+{
+    /// <summary>
+    /// <see cref="IEnumEntity{TKey}"/> Configuration
+    /// </summary>
+    /// <typeparam name="TEnum">Enum type</typeparam>
+    /// <typeparam name="TEnumEntity">Enum lookup table type</typeparam>
+    public class EnumEntityConfiguration<TEnum, TEnumEntity> : IEntityTypeConfiguration<TEnumEntity>
+        where TEnum : struct, IConvertible, IComparable
+        where TEnumEntity : class, IEnumEntity<TEnum>
+    {
+        /// <summary>
+        /// Configures enum entity lookup database table
+        /// </summary>
+        /// <param name="builder">Table builder</param>
+        public void Configure(EntityTypeBuilder<TEnumEntity> builder)
+        {
+            builder.HasKey(i => i.Id);
+
+            builder.Property(i => i.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
